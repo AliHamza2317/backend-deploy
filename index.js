d21f78bda398d267b72c9a30a0de756a088a058e
AliHamza2317 <alihamza707580@gmail.com>
2025-01-18 04:21:38 +0500

    push

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+const port = 9000
+app.use('/', (req, res) => {
+    
+    res.send({message: 'Hello World'})})
+app.listen(9000, () => {
+    console.log(`Server is running on port ${port}`)
+    })
