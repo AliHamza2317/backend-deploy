f65355e38087aa6e09760d3d7eb4e2f80bf40d7b
AliHamza2317 <alihamza707580@gmail.com>
2025-01-19 02:36:45 +0500

    added new one

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const express= require("express");
-
-
-const app = express();
+const express=require ("express");
+const uRoutes = require("./Routes/pupmRoutes");
+const app=express();
+const bodyParser = require("body-parser")
 const cors = require("cors")
-const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-const uRoutes = require("./Routes/pupmRoutes");
-const allowedOrigins = [
-    'https://pump-hicg.vercel.app',
-    'https://pump-6vzl.vercel.app',
-    'http://localhost:3000', // For local development
-  ];
-  
-  // Use dynamic origin handling
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  }));
+
+require("dotenv").config();
+
+
+app.use(cors())
 app.use(express.json())
 
 
