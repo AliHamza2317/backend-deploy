b8027767cc668619e205037c72ff4e03c45ce3a2
AliHamza2317 <alihamza707580@gmail.com>
2025-01-19 02:33:44 +0500

    added

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express= require("express");
-const bodyParser = require('body-parser');
-const cors = require("cors")
+
+
 const app = express();
+const cors = require("cors")
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 const uRoutes = require("./Routes/pupmRoutes");
-app.use(cors({
-    origin: 'https://pump-hicg.vercel.app', // Allow requests only from your frontend
+const allowedOrigins = [
+    'https://pump-hicg.vercel.app',
+    'https://pump-6vzl.vercel.app',
+    'http://localhost:3000', // For local development
+  ];
+  
+  // Use dynamic origin handling
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
   }));
